5533b7b6f73fb2b8a932ba8f24b3a4e2d9a0795e
Richard Neumann <mail@richard-neumann.de>
2022-10-03 16:31:16 +0200

    Add cmpver utility

---
+++ src/cmpver/main.rs
@@ -0,0 +1,30 @@
+use librucman::version::Version;
+use std::cmp::Ordering;
+
+mod args;
+use args::{parse, Args};
+
+fn main() {
+    let args = parse();
+
+    match Version::from_string(args.lhs()) {
+        Ok(lhs) => match Version::from_string(args.rhs()) {
+            Ok(rhs) => {
+                println!(
+                    "{}",
+                    match lhs.cmp(&rhs) {
+                        Ordering::Less => -1,
+                        Ordering::Equal => 0,
+                        Ordering::Greater => 1,
+                    }
+                );
+            }
+            Err(err) => {
+                eprintln!("Invalid version: {} ({})", args.rhs(), err);
+            }
+        },
+        Err(err) => {
+            eprintln!("Invalid version: {} ({})", args.rhs(), err);
+        }
+    }
+}
